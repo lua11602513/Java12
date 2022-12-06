e4bdff4c939f6b13b26f5e0d298e3b36792cbec7
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-07 00:02:50 +0800

    Add files via upload

---
+++ Java14.java
@@ -0,0 +1,8 @@
+
+public class Java14 {
+	public static void main(String args[]) {
+		String str="Habit is second nature.";
+		System.out.println("string="+str);
+		System.out.println("sub-string="+str.substring(16,22));
+ }
+}
