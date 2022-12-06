8fa7e57ef284b96ae94f3ef3a4896733b7a94daf
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-07 00:03:55 +0800

    Add files via upload

---
+++ Java15.java
@@ -0,0 +1,8 @@
+public class Java15{
+	 public static void main(String args[]){
+		 String str;
+		 str=Float.toString(123.45f);
+		 System.out.println(str);
+		 System.out.println("length="+str.length());
+}
+ }
