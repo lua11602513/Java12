0fb19945349468204fc81b017394b8c42da9d8b5
lua11602513 <113486725+lua11602513@users.noreply.github.com>
2022-12-06 20:56:53 +0800

    Delete hw12_8 directory

--- hw12_8/pack6/subpack1/subsubpack/CStyle.java
+++
@@ -1,8 +0,0 @@
-package pack6.subpack1.subsubpack;
-public class CStyle {
-public int style;
-public CStyle(int n){
-style=n;
-System.out.println("style"+style);
-}
- }
--- hw12_8/pack6/subpack2/CRectangle.java
+++
@@ -1,6 +0,0 @@
-package pack6.subpack2;
-public class CRectangle {
-  public void show() {
-	System.out.println("show() method of class CRectangle called");
-}
-}
